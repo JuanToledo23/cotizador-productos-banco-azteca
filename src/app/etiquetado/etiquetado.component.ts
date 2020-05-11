3036a8726a41b25368c5d1bdaf7ec2f6f7bc415a
System Administrator <root@ekt1525746mac.ad.elektra.com.mx>
2020-05-11 11:07:43 -0500

    Cotizador V6

--- src/app/etiquetado/etiquetado.component.ts
+++ src/app/etiquetado/etiquetado.component.ts
@@ -12,18 +12,18 @@ export class EtiquetadoComponent implements OnInit {
   ngOnInit() {
   }
 
-  ngAfterViewInit(): void {
-    document.getElementsByClassName('mat-tab-header-pagination-before')[0].remove();
-    document.getElementsByClassName('mat-tab-header-pagination-after')[0].remove();
-  }
+  // ngAfterViewInit(): void {
+  //   document.getElementsByClassName('mat-tab-header-pagination-before')[0].remove();
+  //   document.getElementsByClassName('mat-tab-header-pagination-after')[0].remove();
+  // }
 
-  articulosEtiquetas = [
-    {id:1, sku:"83990286", descripcion: "Refrigerador Acros Acros", codigo:"83990286", precio:"83990286"},
-    {id:2, sku:"83990286", descripcion: "Refrigerador Acros Acros", codigo:"83990286", precio:"83990286"},
-    {id:3, sku:"83990286", descripcion: "Refrigerador Acros Acros", codigo:"83990286", precio:"83990286"},
-    {id:4, sku:"83990286", descripcion: "Refrigerador Acros Acros", codigo:"83990286", precio:"83990286"},
-    {id:5, sku:"83990286", descripcion: "Refrigerador Acros Acros", codigo:"83990286", precio:"83990286"},
-    {id:6, sku:"83990286", descripcion: "Refrigerador Acros Acros", codigo:"83990286", precio:"83990286"},
-    {id:7, sku:"83990286", descripcion: "Refrigerador Acros Acros", codigo:"83990286", precio:"83990286"}
-  ]
+  // articulosEtiquetas = [
+  //   {id:1, sku:"83990286", descripcion: "Refrigerador Acros Acros", codigo:"83990286", precio:"83990286"},
+  //   {id:2, sku:"83990286", descripcion: "Refrigerador Acros Acros", codigo:"83990286", precio:"83990286"},
+  //   {id:3, sku:"83990286", descripcion: "Refrigerador Acros Acros", codigo:"83990286", precio:"83990286"},
+  //   {id:4, sku:"83990286", descripcion: "Refrigerador Acros Acros", codigo:"83990286", precio:"83990286"},
+  //   {id:5, sku:"83990286", descripcion: "Refrigerador Acros Acros", codigo:"83990286", precio:"83990286"},
+  //   {id:6, sku:"83990286", descripcion: "Refrigerador Acros Acros", codigo:"83990286", precio:"83990286"},
+  //   {id:7, sku:"83990286", descripcion: "Refrigerador Acros Acros", codigo:"83990286", precio:"83990286"}
+  // ]
 }

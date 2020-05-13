48e672f0363a9c3c27139f9eb30b135160165326
Omar <omarquirozhernandez@gmail.com>
2020-05-13 13:42:27 -0500

    Prefinal

--- src/app/tickets-abarrotes/dialogs/dialogDatosGenerales.ts
+++ src/app/tickets-abarrotes/dialogs/dialogDatosGenerales.ts
@@ -3,6 +3,7 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'dialog-DatosGenerales',
     templateUrl: 'dialogDatosGenerales.html',
+    styleUrls: ['ticketsAbarrotes.component.scss'],
 })
 export class DialogDatosGenerales {
     
